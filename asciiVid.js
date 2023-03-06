83e77f2aa755f71ccb740c9a8c9f94286a01b84d
Anthony Marin <90010906+AnthonyMarin@users.noreply.github.com>
2023-03-06 12:36:16 -0800

    Add files via upload

---
+++ asciiVid.js
@@ -0,0 +1,42 @@
+
+const density = '           ..--:;||=++%OO8#@'
+
+// const density = '        .:░▒▓█';
+
+let video;
+let asciiDiv;
+
+function setup() {
+noCanvas();
+
+  video = createCapture(VIDEO);
+  video.size(250, 100);
+  video.hide();
+  asciiDiv = createDiv();
+  asciiDiv.class("digitizer");
+  if(!asciiDiv) console.log("hi");
+
+ 
+}
+
+function draw() {
+  video.loadPixels();
+  let asciiImage = "";
+  for (let j = 0; j < video.height; j++) {
+    for (let i = 0; i < video.width; i++) {
+      const pixelIndex = (i + j * video.width) * 4;
+      const r = video.pixels[pixelIndex + 0];
+      const g = video.pixels[pixelIndex + 1];
+      const b = video.pixels[pixelIndex + 2];
+      const avg = (r + g + b) / 3;
+      const len = density.length;
+      const charIndex = floor(map(avg, 0, 255, 0, len));
+      const c = density.charAt(charIndex);
+      if (c == " ") asciiImage += "&nbsp;";
+      else asciiImage += c;
+    }
+    asciiImage += '<br/>';
+  }
+  asciiDiv.html(asciiImage);
+}
+
